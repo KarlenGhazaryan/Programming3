213d9158b79ff1694ea5ca7398b5775524858afd
KarlenGhazaryan <karlen.ghazaryan.g@tumo.org>
2023-12-05 17:16:23 +0400

    node_modules

---
+++ GameOfLifefinal/express.js
@@ -0,0 +1,15 @@
+// var express = require("express")
+// var app = express()
+
+// app.get("/", function(req, res){
+//     res.send("<h1> Hello student </h1>")
+// })
+
+// app.get("/name/:name", function(req,res){
+//     var name = req.params.name
+//     res.send("<h1> Hello " + name + "</h1>")
+// })
+
+// app.listen(3000, function(){
+//     console.log("express is run 3000 port")
+// })
---
+++ GameOfLifefinal/index.js
@@ -0,0 +1,14 @@
+// console.log("Hello Word")
+
+//os 1 modul
+
+// var os = require("os");
+
+// var message = "this platform is "
+
+// function main(){
+//     console.log(message + os.platform)
+// }
+
+// main()
+
